457048a004f6dd10459c46236f5f662eb47ed0f0
NguyenDucThang201204 <thangnguyenduc2004@gmail.com>
2026-02-26 03:13:55 +0700

    fix(bug): Hotfix

---
+++ KLTN_PlantDecor_BE/PlantDecor.BusinessLogicLayer/DTOs/Updates/PasswordUpdate.cs
@@ -0,0 +1,9 @@
+﻿namespace PlantDecor.BusinessLogicLayer.DTOs.Updates
+{
+    public class PasswordUpdate
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
+    }
+}
